b0bc809c675285c361006ded02e038ca2b5af0e4
Brisingr Aerowing <ztgreve@live.com>
2018-12-07 19:20:55 -0500

    Bumped version

--- src/main/java/tinkersoc/TinkersOCMod.java
+++ src/main/java/tinkersoc/TinkersOCMod.java
@@ -21,7 +21,7 @@ public class TinkersOCMod
 
 	public static final String MODID = "tinkersoc";
 	public static final String NAME = "Tinkers OpenComputers Driver";
-	public static final String VERSION = "0.5";
+	public static final String VERSION = "0.6";
 
 
 	private static Logger logger;
